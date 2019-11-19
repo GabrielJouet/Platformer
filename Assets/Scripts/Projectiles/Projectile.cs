f9d7ef7499d8f784874bc54bab81df5285b393b3
GabrielJouet <32566313+GabrielJouet@users.noreply.github.com>
2019-11-19 20:50:52 +0100

    [Code] Added projectiles and projectile pools
    Also added projectile sprite

---
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+
+    [SerializeField]
+    private float _livingTime;
+
+
+
+    private ProjectilePool _projectilePool;
+
+    private void Start()
+    {
+        _projectilePool = FindObjectOfType<ProjectilePool>();
+        StartCoroutine(FadeOut());
+    }
+
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(transform.position.x + _speed * Time.fixedDeltaTime, 0f, 0f);
+    }
+
+
+    public void Restart()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+
+    private IEnumerator FadeOut()
+    {
+        yield return new WaitForSecondsRealtime(_livingTime);
+        _projectilePool.AddProjectileToList(this);
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Projectiles/ProjectilePool.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectilePool : MonoBehaviour
+{
+    //All projectiles in the scene but not used currently
+    private List<Projectile> _allProjectiles = new List<Projectile>();
+
+
+
+    //Method used when we have to recover or use a projectile
+    public Projectile UseProjectile(Projectile other)
+    {
+        //We set an empty object to null
+        Projectile buffer = null;
+
+        //We will check every object already in the pool to know if the same projectile is already here
+        foreach (Projectile current in _allProjectiles)
+        {
+            //If the projectile already exists
+            if (other.gameObject.name + "(Clone)" == current.gameObject.name)
+            {
+                //We stop the for
+                buffer = current;
+                break;
+            }
+        }
+
+
+        //If we find a projectile
+        if(buffer != null)
+        {
+            //We set it active and remove it from list
+            _allProjectiles.Remove(buffer);
+            buffer.gameObject.SetActive(true);
+        }
+        //Else if we didn't find any projectile
+        else
+        {
+            //We instantiate one
+            buffer = Instantiate(other);
+        }
+
+        return buffer;
+    }
+
+
+
+    //Method used when a projectile is used and its lifespan is finished
+    public void AddProjectileToList(Projectile other)
+    {
+        other.gameObject.SetActive(false);
+        _allProjectiles.Add(other);
+    }
+}
