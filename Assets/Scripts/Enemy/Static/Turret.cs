4cdb6e8f97bcfcf5d628bd57a79c1c41f35d2ed1
WIEREZ Loïc <loic.wierez@viacesi.fr>
2019-11-28 15:12:30 +0100

    [Code] Added simple turret prefab
    Added a turret prefab that can search for the player and aim at him if the angle permit it

--- Assets/Scripts/Enemy/Static/Turret.cs
+++ Assets/Scripts/Enemy/Static/Turret.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class Turret : Enemy
 {
+	private Transform barrelTransform;
+	private float maxAngle = 45.0f;
+
 	private void Start()
 	{
+		barrelTransform = transform.GetChild(0);
 		StartCoroutine("WatchOutPlayer");
 	}
 
 	private void Update()
 	{
+		// If the watching coroutine detect that the turret is seeing the player
 		if (_isChasingPlayer)
 		{
+			// The turret rotate to the direction of the player if the angle is small enough
 			Vector2 PlayerOffset;
+
 			PlayerOffset = _chasingPlayer.transform.position - transform.position;
 			PlayerOffset.Normalize();
-			transform.up = PlayerOffset;
+
+			if (Vector2.Angle(transform.up, PlayerOffset) < maxAngle)
+			{
+				barrelTransform.up = PlayerOffset;
+
+				// And start to attack
+				_isAttackingPlayer = true;
+
+				AttackPlayer();
+
+			}
 		}
+	}
+
+	private void AttackPlayer()
+	{
+
 	}
 }
