cdd2f2de21d0b226889290843cbfd45d31084cd4
WIEREZ Loïc <loic.wierez@viacesi.fr>
2019-11-28 12:46:43 +0100

    Adding basic turret script

---
+++ Assets/Scripts/Enemy/Static/Turret.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : Enemy
+{
+	private void Start()
+	{
+		StartCoroutine("WatchOutPlayer");
+	}
+
+	private void Update()
+	{
+		if (_isChasingPlayer)
+		{
+			Vector2 PlayerOffset;
+			PlayerOffset = _chasingPlayer.transform.position - transform.position;
+			PlayerOffset.Normalize();
+			transform.up = PlayerOffset;
+		}
+	}
+}
