b950937e75491b2d40478531935a64219c964859
GabrielJouet <32566313+GabrielJouet@users.noreply.github.com>
2019-12-10 19:01:49 +0100

    [Code] Add a camera follow prefab for level creating purposes
    Also create the first level (beginning)

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _objectToFollow;
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(_objectToFollow.transform.position.x, _objectToFollow.transform.position.y + 0.5f, -10);
+    }
+}
