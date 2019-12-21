eca9e7372a24b9884f1f8d92361c7dbcb6de933e
WIEREZ Loïc <loic.wierez@viacesi.fr>
2019-12-21 22:19:19 +0100

    [Code] Added frame to camera movement

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,8 +5,28 @@ public class CameraFollow : MonoBehaviour
     [SerializeField]
     private GameObject _objectToFollow;
 
+    private Vector3 _addedMoves;
+
+    private void Start()
+    {
+        transform.position = new Vector3(_objectToFollow.transform.position.x, _objectToFollow.transform.position.y + 0.5f, -50);
+        _addedMoves = new Vector3();
+    }
+
     private void FixedUpdate()
     {
-        transform.position = new Vector3(_objectToFollow.transform.position.x, _objectToFollow.transform.position.y + 0.5f, -10);
+        Vector3 newPosition = new Vector3(_objectToFollow.transform.position.x, _objectToFollow.transform.position.y + 0.5f, -50);
+        Vector3 moveVector = newPosition - transform.position;
+        _addedMoves += moveVector;
+
+        if (_addedMoves.magnitude > 5)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, newPosition, 0.1f);
+        }
+
+        if (moveVector == Vector3.zero)
+        {
+            _addedMoves = new Vector3();
+        }
     }
 }
