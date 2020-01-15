9bdde1d4a5256cde107c83b8d407da9486f3bf46
WIEREZ Loïc <loic.wierez@viacesi.fr>
2020-01-15 15:20:39 +0100

    [Code] Sarting tracking function

--- Assets/Scripts/Enemy/Moving/Flying/Seeker/Seeker.cs
+++ Assets/Scripts/Enemy/Moving/Flying/Seeker/Seeker.cs
@@ -4,4 +4,13 @@ using UnityEngine;
 
 public class Seeker : FlyingEnemy
 {
+	//Common function for all seeker projectiles. Here the object will track the player
+	protected void FollowPlayer()
+	{
+		if (_chasingPlayer != null)
+		{
+			Vector2 playerCoordinate = _chasingPlayer.transform.position;
+			Move(playerCoordinate.x, playerCoordinate.y);
+		}
+	}
 }
---
+++ Assets/Scripts/Enemy/Moving/Flying/Seeker/Swarmer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swarmer : Seeker
+{
+	void Start()
+	{
+		StartCoroutine(WatchOutPlayer());
+	}
+
+	void FixedUpdate()
+	{
+		FollowPlayer();
+	}
+}
