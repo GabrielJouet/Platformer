83dfe502bcbc1e80437b733d582b273af0eeb622
DULEU LUCAS <lucas.duleu@viacesi.fr>
2020-01-20 15:40:23 +0100

    Mobile/Trapped Plateform
    - Mobile plateforms are moving with a certain pattern and capable of carrying the player.
    - Trapped plateform are destroyed after a certain time when the player touches the top.

---
+++ Assets/TrappedPlateform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrappedPlateform : MonoBehaviour
+{
+    [SerializeField]
+    private float _destroyTime = 1f; 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(DestroyCoolDown());
+        }
+    }
+
+    private IEnumerator DestroyCoolDown()
+    {
+        yield return new WaitForSeconds(_destroyTime);
+        Destroy(gameObject);
+    }
+}
