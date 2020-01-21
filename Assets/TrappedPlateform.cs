9d9da0c5b78197fabf63d4de271030c22fb8fdc2
DULEU LUCAS <lucas.duleu@viacesi.fr>
2020-01-21 15:54:01 +0100

    [Code] Trapped Plateform Fix
    The trapped plateform now respawn after a given time, so that the player is still able to try again if he fails the jump.

--- Assets/TrappedPlateform.cs
+++ Assets/TrappedPlateform.cs
@@ -5,7 +5,24 @@ using UnityEngine;
 public class TrappedPlateform : MonoBehaviour
 {
     [SerializeField]
-    private float _destroyTime = 1f; 
+    private float _destroyTime = 1f;
+    [SerializeField]
+    private float _respawnTime = 3f;
+    [SerializeField]
+    private float _localXScale = 6.25f;
+    [SerializeField]
+    private float _localYScale = 0.5f;
+
+    private SpriteRenderer _spriteRenderer;
+    private BoxCollider2D _boxCollider;
+    private Rigidbody2D _rigidBody;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _boxCollider = GetComponent<BoxCollider2D>();
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
@@ -17,6 +34,17 @@ public class TrappedPlateform : MonoBehaviour
     private IEnumerator DestroyCoolDown()
     {
         yield return new WaitForSeconds(_destroyTime);
-        Destroy(gameObject);
+        _spriteRenderer.enabled = false;
+        _boxCollider.enabled = false;
+        _rigidBody.simulated = false;
+        StartCoroutine(RespawnCoolDown());
+    }
+
+    private IEnumerator RespawnCoolDown()
+    {
+        yield return new WaitForSeconds(_respawnTime);
+        _spriteRenderer.enabled = true;
+        _boxCollider.enabled = true;
+        _rigidBody.simulated = true;
     }
 }
